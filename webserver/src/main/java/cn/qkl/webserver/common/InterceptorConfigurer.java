3319fcd681fd38f8783fd64545743665b2c8def0
wxynly <2511985806@qq.com>
2023-05-22 11:07:06 +0800

    feat: 去掉全局统一api前缀

--- webserver/src/main/java/cn/qkl/webserver/common/InterceptorConfigurer.java
+++ webserver/src/main/java/cn/qkl/webserver/common/InterceptorConfigurer.java
@@ -1,12 +1,10 @@
 package cn.qkl.webserver.common;
 
-import cn.qkl.common.framework.auth.Role;
 import cn.qkl.common.framework.auth.TokenInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,8 +23,8 @@ public class InterceptorConfigurer implements WebMvcConfigurer {
                 .addPathPatterns(frameworkProperties.getApiPrefix() + "/**");
     }
 
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix(frameworkProperties.getApiPrefix(), c -> c.isAnnotationPresent(Role.class));
-    }
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        configurer.addPathPrefix(frameworkProperties.getApiPrefix(), c -> c.isAnnotationPresent(Role.class));
+//    }
 }
