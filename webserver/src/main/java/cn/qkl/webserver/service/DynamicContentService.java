6f9a79f579cb8c8b78fe515625bd9daaae41ca42
wxynly <2511985806@qq.com>
2023-05-24 15:54:41 +0800

    fix: 过去24小时范围修改

--- webserver/src/main/java/cn/qkl/webserver/service/DynamicContentService.java
+++ webserver/src/main/java/cn/qkl/webserver/service/DynamicContentService.java
@@ -87,6 +87,8 @@ public class DynamicContentService {
     public void dynamicContentListExport(DynamicContentExportDTO dto, HttpServletResponse response) {
         //当前服务器时间
         Date end = new Date();
+        //一天的开始，结果：2023-05-24 00:00:00
+        end = DateUtil.beginOfDay(end);
         //全部
         DateTime start = null;
         if (dto.getTime() == 1) {
