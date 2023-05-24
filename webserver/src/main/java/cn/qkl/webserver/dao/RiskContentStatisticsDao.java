23becf0fff2e1c4b631ce16229ae1faeaf9f2a92
wxynly <2511985806@qq.com>
2023-05-24 17:40:47 +0800

    fix: 去除dateTypeHandle

--- webserver/src/main/java/cn/qkl/webserver/dao/RiskContentStatisticsDao.java
+++ webserver/src/main/java/cn/qkl/webserver/dao/RiskContentStatisticsDao.java
@@ -1,12 +1,8 @@
 package cn.qkl.webserver.dao;
 
-import cn.qkl.common.framework.util.mybatis.DateTypeHandler;
 import cn.qkl.common.repository.mapper.RiskContentStatisticsMapper;
-import cn.qkl.webserver.vo.platform.PlatformContentVO;
-import cn.qkl.webserver.vo.platform.PlatformListVO;
 import cn.qkl.webserver.vo.riskcontentstatistics.RiskContentStatisticsVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.SelectProvider;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
