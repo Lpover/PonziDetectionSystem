5e66c068544bc1e07219c0faf32fdffd97c06145
wxynly <2511985806@qq.com>
2023-06-06 10:22:49 +0800

    add: 用户登录接口修改

--- webserver/src/main/java/cn/qkl/webserver/dto/user/LoginDTO.java
+++ webserver/src/main/java/cn/qkl/webserver/dto/user/LoginDTO.java
@@ -3,6 +3,7 @@ package cn.qkl.webserver.dto.user;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 /**
@@ -18,6 +19,7 @@ public class LoginDTO {
     private String phone;
 
     @ApiModelProperty("密码")
-    @Pattern(regexp = "[A-Za-z0-9]+", message = "密码格式不正确")
+//    @Pattern(regexp = "[A-Za-z0-9]{6,12}", message = "密码格式不正确")
+    @NotNull
     private String pwd;
 }
