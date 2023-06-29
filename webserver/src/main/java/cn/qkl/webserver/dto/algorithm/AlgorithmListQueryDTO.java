9da945a7c6d48c29ca6a4358e606ece3baedafcb
wxynly <2511985806@qq.com>
2023-06-29 17:06:27 +0800

    add: 更新系统使用的算法、添加算法、修改算法

--- webserver/src/main/java/cn/qkl/webserver/dto/algorithm/AlgorithmListQueryDTO.java
+++ webserver/src/main/java/cn/qkl/webserver/dto/algorithm/AlgorithmListQueryDTO.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 @Data
 public class AlgorithmListQueryDTO{
-    @ApiModelProperty("算法类别：0动态风险识别算法 1账户风险识别算法")
+    @ApiModelProperty("算法类别：0动态风险识别算法 1账户风险识别算法 不传获取所有")
     private List<Integer> type;
 }
