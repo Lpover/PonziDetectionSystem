eef7fa7318a50f624d4c1f40ab3bccc1e325a816
邹卓 <495597056@qq.com>
2023-09-06 10:31:02 +0800

    add: 多线程批量固证

--- webserver/src/main/java/cn/qkl/webserver/service/ContentRiskService.java
+++ webserver/src/main/java/cn/qkl/webserver/service/ContentRiskService.java
@@ -2,6 +2,7 @@ package cn.qkl.webserver.service;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.extra.template.TemplateException;
 import cn.hutool.json.JSONUtil;
 import cn.qkl.common.framework.response.PageVO;
 import cn.qkl.common.framework.util.OssUtil;
@@ -19,7 +20,6 @@ import cn.qkl.webserver.dao.EvidenceWebDao;
 import cn.qkl.webserver.dto.contenrisk.ContentBatchEvidenceDTO;
 import cn.qkl.webserver.dto.contenrisk.ContentRiskInfoDTO;
 import cn.qkl.webserver.vo.contentRisk.ContentRiskInfoVO;
-import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.select.QueryExpressionDSL;
@@ -28,7 +28,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
@@ -116,7 +115,7 @@ public class ContentRiskService {
                 ));
     }
 
-    public void batchReinforce(ContentBatchEvidenceDTO dto) throws IOException, NoSuchAlgorithmException {
+    public void batchReinforce(ContentBatchEvidenceDTO dto) {
 
 
         List<Long> contentIdList = dto.getContentIdList();
@@ -129,87 +128,104 @@ public class ContentRiskService {
         for (Content item: contentList) {
             // 更新content表中的取证状态
             contentDao.update(c->c.set(Tables.content.evidenceStatus).equalTo(1).where(Tables.content.id, isEqualTo(item.getId())));
-            EvidenceWeb evidenceWeb = new EvidenceWeb();
-            evidenceWeb.setContentId(item.getId());
-            // content -> json 上传oss
-            String json = JSONUtil.toJsonStr(item.toString());
-//        log.info("file json:", json);
-            byte[] jsonBytes = json.getBytes();
-            String jsonName = "content_" + item.getId().toString() + ".json";
-            MultipartFile multipartFile = new MockMultipartFile(jsonName, jsonName, "application/json", jsonBytes);
-            String webOss = ossUtil.uploadMultipartFile(multipartFile, jsonName);
-            evidenceWeb.setWebOssPath(webOss);
-            evidenceWeb.setUrl(webOss);
-            evidenceWeb.setId(IdUtil.getSnowflakeNextId());
-            evidenceWeb.setName(item.getName());
-            evidenceWeb.setPlatformId(item.getPlatformId());
-            evidenceWeb.setPlatformId(item.getPlatformId());
-            evidenceWeb.setDeleteStatus(0);
-            evidenceWeb.setUpdateTime(new Date());
-            evidenceWeb.setCreateTime(new Date());
-            evidenceWeb.setFrequency(0);
-            evidenceWeb.setContentId(item.getId());
-//            evidenceWeb.setChainId(item.getChainId());
-            evidenceWeb.setPersonnel("from_riskList");
-            evidenceWeb.setEvidenceType(EvidenceTypeEnum.MANUAL.getCode());
-
-            // 得到风险类型字符串
-            List<Long> riskIdList = Arrays.stream(item.getContentTag().split(",")).map(Long::parseLong).collect(Collectors.toList());
-            String riskStr = "";
-            for (Long riskId: riskIdList) {
-                String category = contentRiskDao.selectOne(c -> c.where(Tables.contentRisk.id, isEqualTo(riskId))).getCategory();
-                riskStr = riskStr + category + ",";
-            }
-            riskStr = riskStr.substring(0, riskStr.length() - 1);
-
-            evidenceWeb.setRiskType(riskStr);
-
-            // 计算hash并上链
-            String digest = uploadToChainUtil.calculateHash(multipartFile.getInputStream(), "MD5");
-            uploadToChainUtil.uploadToChain(digest);
-            evidenceWeb.setPackageHash(digest);                     // 文件hash
-            evidenceWeb.setHash(uploadToChainUtil.getTxHash());     // 上链hash
-            evidenceWeb.setChainTime(uploadToChainUtil.getTxTime());    // 上链时间
-            evidenceWeb.setChainId(ChainEnum.XINZHENG.getCode());
-
-            evidenceWebDao.insert(evidenceWeb);
-
-            // 需要设置计数器保证先后关系
-            CountDownLatch latch = new CountDownLatch(1);
-
-            // 生成证书并上传oss
-            SchedulerUtil.commonScheduler.schedule("generateCert", () -> {
+//            onceReinforce(item);
+            // 多线程
+            SchedulerUtil.commonScheduler.schedule("onceReinforce", () -> {
                 try {
-                    String ossPath = evidenceService.generateEvidenceCert(evidenceWeb.getId());
-                    evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.certOssPath).equalTo(ossPath).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
-                    evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.updateTime).equalTo(new Date()).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
-                    evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.evidencePhase).equalTo(2).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
-                } catch (ParserConfigurationException | IOException |
-                         FontFormatException | TemplateException | SAXException e) {
+                    onceReinforce(item);
+                } catch (IOException e) {
                     throw new RuntimeException(e);
-                } finally {
-                    latch.countDown();
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
                 }
             });
+        }
+    }
 
+    // 单次取证不能加多线程，不然会死锁
+    private void onceReinforce(Content item) throws IOException, NoSuchAlgorithmException {
+        EvidenceWeb evidenceWeb = new EvidenceWeb();
+        evidenceWeb.setContentId(item.getId());
+        // content -> json 上传oss
+        String json = JSONUtil.toJsonStr(item.toString());
+//        log.info("file json:", json);
+        byte[] jsonBytes = json.getBytes();
+        String jsonName = "content_" + item.getId().toString() + ".json";
+        MultipartFile multipartFile = new MockMultipartFile(jsonName, jsonName, "application/json", jsonBytes);
+        String webOss = ossUtil.uploadMultipartFile(multipartFile, jsonName);
+        evidenceWeb.setWebOssPath(webOss);
+        evidenceWeb.setUrl(webOss);
+        evidenceWeb.setId(IdUtil.getSnowflakeNextId());
+        evidenceWeb.setName(item.getName());
+        evidenceWeb.setPlatformId(item.getPlatformId());
+        evidenceWeb.setPlatformId(item.getPlatformId());
+        evidenceWeb.setDeleteStatus(0);
+        evidenceWeb.setUpdateTime(new Date());
+        evidenceWeb.setCreateTime(new Date());
+        evidenceWeb.setFrequency(0);
+        evidenceWeb.setContentId(item.getId());
+//            evidenceWeb.setChainId(item.getChainId());
+        evidenceWeb.setPersonnel("from_riskList");
+        evidenceWeb.setEvidenceType(EvidenceTypeEnum.MANUAL.getCode());
+
+        // 得到风险类型字符串
+        List<Long> riskIdList = Arrays.stream(item.getContentTag().split(",")).map(Long::parseLong).collect(Collectors.toList());
+        String riskStr = "";
+        for (Long riskId: riskIdList) {
+            String category = contentRiskDao.selectOne(c -> c.where(Tables.contentRisk.id, isEqualTo(riskId))).getCategory();
+            riskStr = riskStr + category + ",";
+        }
+        riskStr = riskStr.substring(0, riskStr.length() - 1);
+
+        evidenceWeb.setRiskType(riskStr);
+
+        // 计算hash并上链
+        String digest = uploadToChainUtil.calculateHash(multipartFile.getInputStream(), "MD5");
+        uploadToChainUtil.uploadToChain(digest);
+        evidenceWeb.setPackageHash(digest);                     // 文件hash
+        evidenceWeb.setHash(uploadToChainUtil.getTxHash());     // 上链hash
+        evidenceWeb.setChainTime(uploadToChainUtil.getTxTime());    // 上链时间
+        evidenceWeb.setChainId(ChainEnum.XINZHENG.getCode());
+
+        evidenceWebDao.insert(evidenceWeb);
+
+        // 需要设置计数器保证先后关系
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // 生成证书并上传oss
+//        SchedulerUtil.commonScheduler.schedule("generateCert", () -> {
             try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                String ossPath = evidenceService.generateEvidenceCert(evidenceWeb.getId());
+                evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.certOssPath).equalTo(ossPath).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
+                evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.updateTime).equalTo(new Date()).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
+                evidenceWebDao.update(c -> c.set(Tables.evidenceWeb.evidencePhase).equalTo(2).where(Tables.evidenceWeb.id, isEqualTo(evidenceWeb.getId())));
+            } catch (ParserConfigurationException | IOException |
+                     FontFormatException | TemplateException e) {
+                throw new RuntimeException(e);
+            } catch (freemarker.template.TemplateException e) {
+                throw new RuntimeException(e);
+            } catch (org.xml.sax.SAXException e) {
+                throw new RuntimeException(e);
+            } finally {
+                latch.countDown();
             }
+//        });
 
-            // 生成证据包并上传oss
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 生成证据包并上传oss
 //        SchedulerUtil.commonScheduler.schedule("generateEvidencePack", () -> {
 //            try {
-            evidenceService.generateEvidencePack(evidenceWeb.getId());
+        evidenceService.generateEvidencePack(evidenceWeb.getId());
 //            } catch (IOException e) {
 //                throw new RuntimeException(e);
 //            }
 //        });
-            // 切换成 已固证 状态
-            contentDao.update(c->c.set(Tables.content.evidenceStatus).equalTo(2).where(Tables.content.id, isEqualTo(item.getId())));
-        }
+        // 切换成 已固证 状态
+        contentDao.update(c->c.set(Tables.content.evidenceStatus).equalTo(2).where(Tables.content.id, isEqualTo(item.getId())));
     }
-
-
 }
