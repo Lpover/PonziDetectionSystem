155fadc64adfe26720a2d43db54a585c078f1bed
卢鹏程 <11884197+JerryLLL@user.noreply.gitee.com>
2023-10-17 17:17:24 +0800

    FIX：每日插入数据注释

--- webserver/src/main/java/cn/qkl/webserver/backgroundTask/AccountBackgroundTask.java
+++ webserver/src/main/java/cn/qkl/webserver/backgroundTask/AccountBackgroundTask.java
@@ -1,55 +1,55 @@
-package cn.qkl.webserver.backgroundTask;
-
-import cn.qkl.common.framework.initAndBackground.BackgroundTask;
-import cn.qkl.webserver.service.AccountCheckService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.Calendar;
-
-/**
- * @title:
- * @Author sunxiaen
- * @Date: 2023/5/25 15:00
- * @wiki
- */
-@Slf4j
-@Component
-public class AccountBackgroundTask implements BackgroundTask {
-
-    @Resource
-    AccountCheckService accountCheckService;
-
-    /**
-     * 间隔1小时
-     */
-    @Override
-    public long getPeriod() {
-        return 1 * 60 * 60 * 1000;
-    }
-
-    //每天18点执行
-    @Override
-    public long getDelay() {
-        //当前时间
-        Calendar now = Calendar.getInstance();
-        Calendar midnight = Calendar.getInstance();
-        midnight.set(Calendar.HOUR_OF_DAY, 18);
-        midnight.set(Calendar.MINUTE, 0);
-        midnight.add(Calendar.DATE, 1);
-        return midnight.getTime().getTime() - now.getTime().getTime();
-    }
-
-    @Override
-    public String getName() {
-        return AccountBackgroundTask.class.getName();
-    }
-
-    @Override
-    public void run() {
-        log.debug("check account risk");
-        //业务代码
-        //
-    }
-}
+//package cn.qkl.webserver.backgroundTask;
+//
+//import cn.qkl.common.framework.initAndBackground.BackgroundTask;
+//import cn.qkl.webserver.service.AccountCheckService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//import java.util.Calendar;
+//
+///**
+// * @title:
+// * @Author sunxiaen
+// * @Date: 2023/5/25 15:00
+// * @wiki
+// */
+//@Slf4j
+//@Component
+//public class AccountBackgroundTask implements BackgroundTask {
+//
+//    @Resource
+//    AccountCheckService accountCheckService;
+//
+//    /**
+//     * 间隔1小时
+//     */
+//    @Override
+//    public long getPeriod() {
+//        return 1 * 60 * 60 * 1000;
+//    }
+//
+//    //每天18点执行
+//    @Override
+//    public long getDelay() {
+//        //当前时间
+//        Calendar now = Calendar.getInstance();
+//        Calendar midnight = Calendar.getInstance();
+//        midnight.set(Calendar.HOUR_OF_DAY, 18);
+//        midnight.set(Calendar.MINUTE, 0);
+//        midnight.add(Calendar.DATE, 1);
+//        return midnight.getTime().getTime() - now.getTime().getTime();
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return AccountBackgroundTask.class.getName();
+//    }
+//
+//    @Override
+//    public void run() {
+//        log.debug("check account risk");
+//        //业务代码
+//        //
+//    }
+//}
