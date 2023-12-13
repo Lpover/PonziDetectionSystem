6e8f36839623494acaa1d0fe39f94a9f957536d4
卢鹏程 <11884197+JerryLLL@user.noreply.gitee.com>
2023-12-13 15:42:49 +0800

    FIX：动态内容监测历史状态添加

--- webserver/src/main/java/cn/qkl/webserver/service/DetailService.java
+++ webserver/src/main/java/cn/qkl/webserver/service/DetailService.java
@@ -170,7 +170,7 @@ public class DetailService {
     public PageVO<ContentDynamicMonitorVO> getDynamicMonitor(ContentDynamicMonitorDTO dto) {
         return PageVO.getPageData(dto.getPageId(), dto.getPageSize(), () -> contentDao.getDynamicMonitor(
                 select(Tables.dynamicMonitor.id, Tables.dynamicMonitor.riskLevel, Tables.dynamicMonitor.contentTag, Tables.dynamicMonitor.createTime
-                    ,Tables.dynamicMonitor.character)
+                    ,Tables.dynamicMonitor.character,Tables.dynamicMonitor.status)
                         .from(Tables.dynamicMonitor)
                         .where(Tables.dynamicMonitor.contentId, isEqualTo(dto.getContentID()))
                         .orderBy(Tables.dynamicMonitor.createTime.descending()) // 添加排序条件
