ac4b09b198f4bed69d058986883432220c3c2bd9
sunxiaen <sunxiaen@zju.edu.cn>
2023-12-20 14:06:18 +0800

    bugfix 列表排序/空like查询 v0.6.0 日志模块

--- webserver/src/main/java/cn/qkl/webserver/service/LogService.java
+++ webserver/src/main/java/cn/qkl/webserver/service/LogService.java
@@ -1,6 +1,7 @@
 package cn.qkl.webserver.service;
 
 import cn.qkl.common.framework.response.PageVO;
+import cn.qkl.common.framework.util.SqlUtil;
 import cn.qkl.common.repository.Tables;
 import cn.qkl.webserver.dao.LoginLogDao;
 import cn.qkl.webserver.dao.OperationLogDao;
@@ -29,16 +30,17 @@ public class LogService {
 
     public PageVO<LoginLogVO> getLoginLog(LoginLogDTO dto) {
         return new PageVO<>(dto.getPageId(),dto.getPageSize(),() -> loginLogDao.getLoginLogVO(select(Tables.loginLog.id,Tables.user.name.as("userName"),Tables.loginLog.apiName,Tables.loginLog.apiRouter,Tables.loginLog.ip,Tables.loginLog.detail,Tables.loginLog.time).from(Tables.loginLog).leftJoin(Tables.user).on(Tables.user.id, equalTo(Tables.loginLog.userId))
-                .where(Tables.user.name, isLikeCaseInsensitiveWhenPresent("%"+dto.getUserName()+"%"))
+                .where(Tables.user.name, isLikeCaseInsensitiveWhenPresent(SqlUtil.allLike(dto.getUserName())))
                 .and(Tables.loginLog.time, isLessThanOrEqualToWhenPresent(dto.getEndTime()))
-                .and(Tables.loginLog.time, isGreaterThanOrEqualToWhenPresent(dto.getStartTime())).build().render(RenderingStrategies.MYBATIS3)));
-
+                .and(Tables.loginLog.time, isGreaterThanOrEqualToWhenPresent(dto.getStartTime()))
+                .orderBy(Tables.loginLog.time.descending()).build().render(RenderingStrategies.MYBATIS3)));
     }
 
     public PageVO<OperationLogVO> getOperationLog(OperationLogDTO dto) {
         return new PageVO<>(dto.getPageId(),dto.getPageSize(),() -> operationLogDao.getOperationLogVO(select(Tables.operationLog.id,Tables.user.name.as("userName"),Tables.operationLog.apiName,Tables.operationLog.apiRouter,Tables.operationLog.ip,Tables.operationLog.time).from(Tables.operationLog).leftJoin(Tables.user).on(Tables.user.id, equalTo(Tables.operationLog.userId))
-                .where(Tables.user.name, isLikeCaseInsensitiveWhenPresent("%"+dto.getUserName()+"%"))
+                .where(Tables.user.name, isLikeCaseInsensitiveWhenPresent(SqlUtil.allLike(dto.getUserName())))
                 .and(Tables.operationLog.time, isLessThanOrEqualToWhenPresent(dto.getEndTime()))
-                .and(Tables.operationLog.time, isGreaterThanOrEqualToWhenPresent(dto.getStartTime())).build().render(RenderingStrategies.MYBATIS3)));
+                .and(Tables.operationLog.time, isGreaterThanOrEqualToWhenPresent(dto.getStartTime()))
+                .orderBy(Tables.operationLog.time.descending()).build().render(RenderingStrategies.MYBATIS3)));
     }
 }
