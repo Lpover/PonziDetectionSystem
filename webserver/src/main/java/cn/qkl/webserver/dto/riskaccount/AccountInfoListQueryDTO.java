53170f6c4989661fb78d17df32478a9d09a24328
sunxiaen <sunxiaen@zju.edu.cn>
2023-12-25 16:06:29 +0800

    bugfix 风险监测与识别-账户监测-风险评级-筛选功能异常

--- webserver/src/main/java/cn/qkl/webserver/dto/riskaccount/AccountInfoListQueryDTO.java
+++ webserver/src/main/java/cn/qkl/webserver/dto/riskaccount/AccountInfoListQueryDTO.java
@@ -7,16 +7,15 @@ import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Size;
-import java.util.Date;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AccountInfoListQueryDTO extends PageDTO {
 
-    @ApiModelProperty("风险评级：1低风险 2中风险 3高风险, 不传表示都选")
-    @Size(max = 3)
-    private List<@Range(min = 1, max = 3) Integer> riskLevelList;
+    @ApiModelProperty("风险评级：0无风险 1低风险 2中风险 3高风险, 不传表示都选")
+    @Size(max = 4)
+    private List<@Range(min = 0, max = 3) Integer> riskLevelList;
 
     @ApiModelProperty("关联数字内容最小值")
     private Integer relatedNumMin;
