4d5b88cbe0f05d917631b66af03a88098de828c0
卢鹏程 <11884197+JerryLLL@user.noreply.gitee.com>
2023-12-26 17:20:24 +0800

    FIX：平台风险指数变化趋势视图

--- webserver/src/main/java/cn/qkl/webserver/dto/platformview/PlatformAndTimeSelectionDTO.java
+++ webserver/src/main/java/cn/qkl/webserver/dto/platformview/PlatformAndTimeSelectionDTO.java
@@ -9,7 +9,7 @@ public class PlatformAndTimeSelectionDTO {
     @ApiModelProperty("平台选择：平台ID")
     private long selectPlatformId=-999;//-999代表未选择
 
-    @ApiModelProperty("时间范围：1-7天 2-30天 默认7天")
+    @ApiModelProperty("时间范围：1-7天 2-30天 3-180天，默认7天")
     private long selectTime=1;
 
 }
