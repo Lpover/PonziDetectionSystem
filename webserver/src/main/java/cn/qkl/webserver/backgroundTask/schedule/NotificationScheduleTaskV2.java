ff04e136c81f9e04834ffe1ea146d4006d77abe8
sunxiaen <sunxiaen@zju.edu.cn>
2024-01-04 17:11:44 +0800

    通知模块定时逻辑整体重构

--- webserver/src/main/java/cn/qkl/webserver/backgroundTask/schedule/NotificationScheduleTaskV2.java
+++ webserver/src/main/java/cn/qkl/webserver/backgroundTask/schedule/NotificationScheduleTaskV2.java
@@ -1,9 +1,5 @@
 package cn.qkl.webserver.backgroundTask.schedule;
 
-/**
- * @Author sunxiaen
- * @Date 2023/8/28 15:45
- **/
 import cn.hutool.core.util.IdUtil;
 import cn.qkl.common.framework.util.FunctionUtil;
 import cn.qkl.common.repository.Tables;
@@ -16,68 +12,50 @@ import cn.qkl.webserver.dao.NotifyRecordDao;
 import cn.qkl.webserver.dao.SwitchTableDao;
 import cn.qkl.webserver.dao.ThresholdsDao;
 import cn.qkl.webserver.service.kafka.WarnService;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
-import org.springframework.scheduling.Trigger;
-import org.springframework.scheduling.TriggerContext;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Nullable;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
 import static org.mybatis.dynamic.sql.SqlBuilder.isGreaterThanOrEqualTo;
 
 /**
- * 定时任务
- */
-@Data
-@Slf4j
+ * @Author sunxiaen
+ * @Date 2024/1/4 16:18
+ **/
 @Component
-public class NotificationScheduleTask implements SchedulingConfigurer {
+@Slf4j
+public class NotificationScheduleTaskV2 {
 
-    // 配置文件中配置表达式，默认是0/15 * * * * ?（15秒执行一次）
-    // @Value("${notification.cron:0 */10 * * * ?}")
-    // 这个项目的话记录在mysql的switch表中
-    private String cron;
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private Runnable task;
+    private long initialDelay = 0; //默认scheduler立即启动
+    private long period = 15; // 默认时间间隔为15秒
 
-    @Autowired
-    ThresholdsDao thresholdsDao;
+    private boolean running = false;
 
-    @Autowired
-    SwitchTableDao switchTableDao;
+    public boolean checkRunning() {
+        return running;
+    }
 
-    @Autowired
-    NotifyRecordDao notifyRecordDao;
+    public NotificationScheduleTaskV2(SwitchTableDao switchTableDao, ThresholdsDao thresholdsDao, NotifyRecordDao notifyRecordDao, WarnService warnService) {
+        Optional<SwitchTable> config =  switchTableDao.selectOne(c -> c
+                .where(Tables.switchTable.open,isEqualTo(SwitchEnum.ON.getCode()))
+        );
+        config.ifPresent(switchTable -> period = switchTable.getFrequency() * 60L);
 
-    @Autowired
-    WarnService warnService;
-
-    @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-
-        Optional<SwitchTable> config =  switchTableDao.selectOne(c -> c);
-        Integer frequency = config.get().getFrequency();
-        String cron = String.format("0 */%d * * * ?", frequency);
-        this.setCron(cron);
-        System.out.println(cron);
-
-        // 动态使用cron表达式设置循环间隔
-        taskRegistrar.addTriggerTask(() -> {
-            // todo 任务
+        task = () -> {
+            // 执行定时任务的操作
             log.info("Current time： {}", LocalDateTime.now());
             log.debug("check notice");
-            log.debug("check cron: " + this.cron);
+            log.debug("check period: " + period + "seconds");
             //业务代码
             Optional<SwitchTable> st =  switchTableDao.selectOne(c -> c
                     .where(Tables.switchTable.open,isEqualTo(SwitchEnum.ON.getCode()))
@@ -138,14 +116,34 @@ public class NotificationScheduleTask implements SchedulingConfigurer {
             });
             notifyRecordDao.insert(obj);
             warnService.tsgzWarn(obj);
-        }, new Trigger() {
-            @Override
-            public Date nextExecutionTime(@NonNull TriggerContext triggerContext) {
-                // 使用CronTrigger触发器，可动态修改cron表达式来操作循环规则
-                CronTrigger cronTrigger = new CronTrigger(NotificationScheduleTask.this.cron);
-                System.out.println(NotificationScheduleTask.this.cron);
-                return cronTrigger.nextExecutionTime(triggerContext);
-            }
-        });
+        };
+        startTask();
+    }
+
+    public boolean isRunning() {
+        return scheduler.isShutdown();
+    }
+    public synchronized void startTask() {
+        if (!running) {
+            running = true;
+            scheduler.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+        }
+    }
+
+    public synchronized void stopTask() {
+        if (running) {
+            scheduler.shutdown();
+            running = false;
+        }
+    }
+
+    public synchronized void changePeriod(long newPeriod) {
+        // 动态修改时间间隔
+        period = newPeriod;
+        // 取消当前的任务，如有
+        stopTask();
+        // 重新启动任务
+        scheduler = Executors.newScheduledThreadPool(1);
+        startTask();
     }
 }
