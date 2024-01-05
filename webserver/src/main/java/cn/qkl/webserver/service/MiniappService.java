75d3666327a616a9074243c0b7cb147a42ceabab
lpc <lpc2010@126.com>
2024-01-05 11:40:15 +0800

    ADD:证据包生成

--- webserver/src/main/java/cn/qkl/webserver/service/MiniappService.java
+++ webserver/src/main/java/cn/qkl/webserver/service/MiniappService.java
@@ -6,6 +6,7 @@ import cn.qkl.common.framework.util.FunctionUtil;
 import cn.qkl.common.framework.util.OssUtil;
 import cn.qkl.common.framework.util.SnowflakeIdUtil;
 import cn.qkl.common.repository.Tables;
+import cn.qkl.common.repository.model.EvidenceWeb;
 import cn.qkl.common.repository.model.MiniappEvidence;
 import cn.qkl.webserver.common.enums.MiniappEvidenceEnum;
 import cn.qkl.webserver.dao.MiniappEvidenceDao;
@@ -13,23 +14,26 @@ import cn.qkl.webserver.dto.miniapp.EvidenceDTO;
 import cn.qkl.webserver.dto.miniapp.VideoUploadDTO;
 import cn.qkl.webserver.vo.miniapp.EvidenceVO;
 import cn.qkl.webserver.vo.miniapp.VideoVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
 import org.mp4parser.boxes.iso14496.part12.MovieBox;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.mp4parser.IsoFile;
 
+import static cn.qkl.common.framework.util.OssUtil.log;
 import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 /**
@@ -37,6 +41,7 @@ import static org.mybatis.dynamic.sql.SqlBuilder.*;
  * @Date 2024/1/2 14:09
  **/
 
+@Slf4j
 @Service
 public class MiniappService {
 
@@ -47,7 +52,7 @@ public class MiniappService {
     MiniappEvidenceDao miniappEvidenceDao;
 
     public VideoVO videoUpload(VideoUploadDTO dto) throws IOException {
-        String ossUrl = ossUtil.uploadMultipartFile(dto.getFile(),dto.getName());
+        String ossUrl = ossUtil.uploadMultipartFile(dto.getFile(),dto.getFile().getOriginalFilename());
         MiniappEvidence evidence = new MiniappEvidence();
         evidence.setId(SnowflakeIdUtil.generateId());
         evidence.setName(dto.getName());
@@ -122,5 +127,78 @@ public class MiniappService {
             return null;
         }
         return null;
+    }
+
+    public void generateEvidencePack(Long id) throws IOException {
+        Optional<MiniappEvidence> miniappEvidence = miniappEvidenceDao.selectOne(c->c
+                .where(Tables.miniappEvidence.id, isEqualTo(id)));
+        String webOssPath= miniappEvidence.get().getUrl();
+        if (webOssPath == null || webOssPath.isEmpty() ) {
+            log.error("缺少OSS地址");
+            return;
+        }
+
+        String ext = "";
+        int lastDotIndex = webOssPath.lastIndexOf(".");
+        if (lastDotIndex != -1 && lastDotIndex < webOssPath.length() - 1) {
+            ext = webOssPath.substring(lastDotIndex + 1);
+        }
+
+        String zipFilePath = "pack" + id + ".zip";
+        try {
+            InputStream webStream = ossUtil.downloadFileByURL(webOssPath);
+
+            try (FileOutputStream fos = new FileOutputStream(zipFilePath);
+                 ZipOutputStream zipOut = new ZipOutputStream(fos)) {
+                compressPack(webStream, ext, zipOut, id);
+            } catch (IOException e) {
+                // 处理文件操作过程中的 IO 异常
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (webStream != null) {
+                        webStream.close();
+                    }
+                } catch (IOException e) {
+                    // 处理关闭流时的异常
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            // 处理其他异常
+            e.printStackTrace();
+        }
+
+        File zipFile = new File(zipFilePath);
+        FileInputStream fileInputStream = null;
+        MultipartFile multipartFile = null;
+        try {
+            fileInputStream = new FileInputStream(zipFile);
+            multipartFile = new MockMultipartFile(zipFile.getName(),zipFile.getName(),
+                    ContentType.APPLICATION_OCTET_STREAM.toString(),fileInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String packOssPath = ossUtil.uploadMultipartFile(multipartFile, zipFilePath);
+        fileInputStream.close();
+        zipFile.delete();
+        evidenceWebDao.update(c ->
+                c.set(Tables.evidenceWeb.packOssPath).equalTo(packOssPath)
+                        .set(Tables.evidenceWeb.updateTime).equalTo(new Date())
+                        .set(Tables.evidenceWeb.evidencePhase).equalTo(2)
+                        .where(Tables.evidenceWeb.id, isEqualTo(id))
+        );
+        log.info("生成证据包完成");
+    }
+
+    private void compressPack(InputStream webStream, String ext, ZipOutputStream zipOut, Long id) throws IOException {
+        zipOut.putNextEntry(new ZipEntry("Vedio" + ext + ".mp4"));
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = webStream.read(buffer)) > 0) {
+            zipOut.write(buffer, 0, len);
+        }
+        zipOut.closeEntry();
+
     }
 }
